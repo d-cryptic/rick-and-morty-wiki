6351b2a07756369769f680e1671eddde78d69096
Barun Debnath <barundebnath91@gmail.com>
2022-01-18 15:18:18 +0530

    card grid placed

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap";
+import Cards from "./components/Cards/Cards";
+import Filters from "./components/Filters/Filters";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-center ubuntu my-4">
+        React & Morty <span className="text-primary">Wiki</span>
+      </h1>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-3">
+            <Filters />
+          </div>
+          <div className="col-8">
+            <div className="row">
+              <Cards />
+              <Cards />
+              <Cards />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
