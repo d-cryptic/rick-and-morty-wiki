2fd05f52a7810f70ea89d72ceab6a2f1b31cb86f
Barun Debnath <barundebnath91@gmail.com>
2022-01-19 17:12:18 +0530

    react router and navbar

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,29 @@ import Cards from "./components/Cards/Cards";
 import Filters from "./components/Filters/Filters";
 import Pagination from "./components/Pagination/Pagination";
 import Search from "./components/Search/Search";
+import Navbar from "./components/Navbar/Navbar";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Episodes from "./components/Pages/Episodes";
+import Location from "./components/Pages/Location";
 
 function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+      </div>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/episodes" element={<Episodes />} />
+        <Route path="/location" element={<Location />} />
+      </Routes>
+    </Router>
+  );
+}
+
+const Home = () => {
   let [pageNumber, setPageNumber] = useState(1);
   let [search, setSearch] = useState("");
   let [status, setStatus] = useState("");
@@ -28,10 +49,6 @@ function App() {
 
   return (
     <div className="App">
-      <h1 className="text-center ubuntu my-4">
-        React & Morty <span className="text-primary">Wiki</span>
-      </h1>
-
       <Search setSearch={setSearch} setPageNumber={setPageNumber} />
 
       <div className="container">
@@ -57,6 +74,6 @@ function App() {
       />
     </div>
   );
-}
+};
 
 export default App;
